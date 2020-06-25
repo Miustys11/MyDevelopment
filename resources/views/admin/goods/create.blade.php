7408413e8e810225c85725402d846431ab8f8d9c
Miu Satou <miustys11@gmail.com>
2020-06-25 04:52:00 +0000

    admins情報を追加

--- resources/views/admin/goods/create.blade.php
+++ resources/views/admin/goods/create.blade.php
@@ -15,7 +15,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 mx-auto">
-                        <h2>商品詳細a</h2>
+                        <h2>商品詳細</h2>
                         <form action="{{ action('Admin\GoodsController@create') }}" method="post" enctype="multipart/form-data">
                              @if (count($errors) > 0)
                                 <ul>
