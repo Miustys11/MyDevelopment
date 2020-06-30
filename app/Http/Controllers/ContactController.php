d05be0e822164c9ec0cf81fba8c853ef38787f5f
Miu Satou <miustys11@gmail.com>
2020-06-30 05:57:12 +0000

    contact/index.blade.phpに内容記述

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    
+    public function index() {
+        
+        //フォーム入力画ページのviewを表示
+        return view('contact.index');
+    }
+
+    public function confirm(Request $request) {
+        
+    }
+
+    public function send(Request $request) {
+        
+    }
 }
