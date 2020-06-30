53f4735369bc5ff180c14d1b762e839d9bca00c9
Miu Satou <miustys11@gmail.com>
2020-06-30 06:13:18 +0000

    ContactSendmail作成

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -15,6 +15,18 @@ class ContactController extends Controller
 
     public function confirm(Request $request) {
         
+        //バリデーションを実行（結果に問題があれば処理を中断してエラーを返す）
+        $request->validate([
+            'email' => 'required|email',
+            'title' => 'required',
+            'body'  => 'required',
+        ]);
+        
+        //フォームから受け取ったすべてのinputの値を取得
+        $inputs = $request->all();
+        
+        //入力内容確認ページのviewに変数を渡して表示
+        return view('contact.confirm', ['inputs' => $inputs]);
     }
 
     public function send(Request $request) {
